7b06a60f8c5cbbbe50d21cd48f58476ea03df191
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 11:05:30 -0300

    adicionado o cm nos inputs

--- questao_6.py
+++ questao_6.py
@@ -1,5 +1,5 @@
 #Escreva um programa que receba a base e a altura de um triangulo e calcule a sua área.
-base=float(input("Digite o valor da base de um triangulo:"))
-altura=float(input("digite o valor da altura de um triangulo:"))
+base=float(input("Digite o valor da base de um triangulo (cm):"))
+altura=float(input("digite o valor da altura de um triangulo (cm):"))
 area=float(base*altura)/2
 print(f"o valor da area do triangulo é:{area}")
