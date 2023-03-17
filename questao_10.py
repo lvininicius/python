f039e49b6a66546fdacaee5ccb30b1b851c422c5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 11:43:37 -0300

    add exercicio 10

---
+++ questao_10.py
@@ -0,0 +1,6 @@
+n=int(input("Numero 3 digitos:"))
+a=n//100
+r=n%100
+b=r//10
+c=r%10
+print(f"Numero invertido: {c}{b}{a}")
---
+++ questao_9.py
@@ -0,0 +1,7 @@
+a=int(input("valor dE A:"))
+b=int(input("valor de B:"))
+c=a
+a=b
+b=c
+print(f"valor de a:{a}")
+print(f"Valor de B:{b}")
