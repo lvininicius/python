9a7524ca2cd8dac3d01cddfb9566132fb9eec0e2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 11:28:10 -0300

    Adicionando valores as variaveis

--- questao_8.py
+++ questao_8.py
@@ -1,3 +1,9 @@
-salario=2.500
+salario=2500
 nome=str(input("Digite o nome do vendedor:"))
-vendas=int(input("digite a quantidade de carros vendidos:"))S
+vendas=int(input("digite a quantidade de carros vendidos:"))
+valor=float(input("valor total das vendas:"))
+
+comissao=200*vendas
+acrescimo=valor*2/100
+d=salario+comissao+acrescimo
+print(f"Salario do vendedor {nome} é {d}")
