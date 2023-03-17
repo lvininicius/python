1289ff0161d030282100e83243dc4b96e45c0abd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 10:45:39 -0300

    adicionado alterações da correção

--- questao_2.py
+++ questao_2.py
@@ -1,5 +1,5 @@
-a=int(input("Digite um valor:"))
-b=int(input("digite outro valor:"))
+a=float(input("Digite um valor:"))
+b=float(input("digite outro valor:"))
 c=a+b
 d=a*b
 print(f"O a soma dos valores informado é:{c}")
--- questao_3.py
+++ questao_3.py
@@ -1,5 +1,5 @@
-a=int(input("Digite a nota-1:"))
-b=int(input("digite a nota-2:"))
-c=int(input("digite a nota-3:"))
+a=float(input("Digite a nota-1:"))
+b=float(input("digite a nota-2:"))
+c=float(input("digite a nota-3:"))
 d=(a+b+c)/3
-print(f"O a média da sua nota é:{d}")
+print(f"O a média da sua nota é:{round(d,2)}")
