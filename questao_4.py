49613e718936d8f4eaa78fe7f601250c84a6963c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 10:50:45 -0300

    adicionado comentarios e corrigido o tipo da variavel

--- questao_4.py
+++ questao_4.py
@@ -1,3 +1,3 @@
 temp=int(input("digite a temperuta em celsius:"))
 f=temp*1.8+32
-print(f"A a temperatura informada em Fahrenheit é:{f}F")
+print(f"A a temperatura informada em Fahrenheit é:{f} F°")
