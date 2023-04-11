1bd58e3249790617deb5b84d40a86a71263340a9
Luca Vinicius <lvininicius@gmail.com>
2023-04-11 09:50:24 -0300

    add o ex4

---
+++ Python0411/ex4.py
@@ -0,0 +1,11 @@
+'''
+Ecreva um altoritmo que o
+'''
+cont=1
+quantidade=0
+while cont <=10:
+    numero = float(input("numero: "))
+    if numero >=100 and numero  <=200:
+        quantidade +=1
+    cont +=1
+print(quantidade)
